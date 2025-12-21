a3373a3968b3c156331f9376c6a614e8d91abbb7
Michael Leggo <mleggo1@hotmail.com>
2025-12-21 09:41:21 +0800

    fix: change base path to root for Vercel deployment and update output directory

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ETF-Dashboard/",
+  base: "/",
   build: {
-    outDir: "docs",
+    outDir: "dist",
   },
   plugins: [react()],
 });
